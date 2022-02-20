78b7081a2fef41fb1c611e0ebacab457259608e1
Niander Wallace | Бегущий по лезвию <45773835+solarjin@users.noreply.github.com>
2022-02-21 02:10:29 +0300

    Саня дал 3 файла отредактированных

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,30 @@
-$(function () {
+$(document).ready(function() {
 
-    $("#phone").mask("+7(999) 999-9999");
-    $("#phone2").mask("+7(999) 999-9999");
-});
+  $.fn.menumaker = function(options) {
+    var cssmenu = $(this), settings = $.extend({
+      title: "Menu",
+      format: "dropdown",
+      sticky: false
+    }, options);
 
-(function($) {
-    $.fn.menumaker = function(options) {      
-      var cssmenu = $(this), settings = $.extend({
-        title: "Menu",
-        format: "dropdown",
-        sticky: false
-      }, options);
-   
-      return this.each(function() {
-        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
-        $(this).find("#menu-button").on('click', function(){
-          $(this).toggleClass('menu-opened');
-          var mainmenu = $(this).next('ul');
-          if (mainmenu.hasClass('open')) { 
-            mainmenu.hide().removeClass('open');
+    return this.each(function() {
+      cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+      $(this).find("#menu-button").on('click', function(){
+        $(this).toggleClass('menu-opened');
+        var mainmenu = $(this).next('ul');
+        if (mainmenu.hasClass('open')) {
+          mainmenu.hide().removeClass('open');
+        }
+        else {
+          mainmenu.show().addClass('open');
+          if (settings.format === "dropdown") {
+            mainmenu.find('ul').show();
           }
-          else {
-            mainmenu.show().addClass('open');
-            if (settings.format === "dropdown") {
-              mainmenu.find('ul').show();
-            }
-          }
+        }
       });
-   
+
       cssmenu.find('li ul').parent().addClass('has-sub');
-   
+
       multiTg = function() {
         cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
         cssmenu.find('.submenu-button').on('click', function() {
@@ -42,32 +37,45 @@ $(function () {
           }
         });
       };
-   
+
       if (settings.format === 'multitoggle') multiTg();
       else cssmenu.addClass('dropdown');
-   
+
       if (settings.sticky === true) cssmenu.css('position', 'fixed');
-   
+
       resizeFix = function() {
         if ($( window ).width() > 768) {
-          cssmenu.find('ul').show();
+          // cssmenu.find('ul').show();
         }
-   
+
         if ($(window).width() <= 768) {
           cssmenu.find('ul').hide().removeClass('open');
         }
       };
       resizeFix();
       return $(window).on('resize', resizeFix);
-   
+
     });
-   };
-  })(jQuery);
-  (function($){
-  $(document).ready(function(){
+  };
+
+
+
+
+
   $("#cssmenu").menumaker({
-     title: "Menu",
-     format: "multitoggle"
+    title: "Menu",
+    format: "multitoggle"
   });
-  });
-  })(jQuery);
+
+
+
+
+
+
+
+
+
+  $("#phone").mask("+7(999) 999-9999");
+  $("#phone2").mask("+7(999) 999-9999");
+
+})
